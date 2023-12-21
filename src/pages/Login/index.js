dce94b90fc63ba7db2ec0ea183b11bf6760e99fa
Xin Fang <fangxin1996@gmail.com>
2023-12-20 19:46:16 -0500

    update login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
 
-export default Login
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form
+          validateTrigger="onBlur"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号码格式不对",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
